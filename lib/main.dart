da3b722662808139a0d2b4433b7ddaa848bf5c80
Ratchanon Pheungta <ratchanon@topgunthailand.com>
2023-09-13 09:54:20 +0700

    complete

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+            drawerTheme: const DrawerThemeData(scrimColor: Colors.transparent)),
         title: 'Flutter Demo',
         home: const MyHomePage(title: 'Flutter Demo Home Page'),
         routes: {
@@ -34,147 +36,239 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+  final GlobalKey<ScaffoldState> _globalKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xFF171717),
-      body: Stack(
-        children: [
-          Column(children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 70, left: 5, right: 5),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.menu,
-                        color: Colors.white,
-                      )),
-                  IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.search,
-                        color: Colors.white,
-                      ))
-                ],
-              ),
-            ),
-            SizedBox(
-              height: 50,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                padding: const EdgeInsets.only(left: 10),
-                children: [
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text("Messages",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold)),
-                  ),
-                  const SizedBox(width: 35),
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text("Online",
-                        style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold)),
-                  ),
-                  const SizedBox(width: 35),
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text("Group",
-                        style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold)),
-                  ),
-                  const SizedBox(width: 35),
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text("More",
-                        style: TextStyle(
-                            color: Colors.grey,
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold)),
-                  ),
-                  const SizedBox(width: 35),
-                ],
-              ),
-            ),
-          ]),
-          Positioned(
-              top: 190,
-              left: 0,
-              right: 0,
-              child: Container(
-                height: 220,
-                padding: const EdgeInsets.only(top: 15, left: 25, right: 25),
-                decoration: const BoxDecoration(
-                    color: Colors.cyan,
-                    borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(40),
-                        topRight: Radius.circular(40))),
-                child: Column(
+        key: _globalKey,
+        backgroundColor: const Color(0xFF171717),
+        body: Stack(
+          children: [
+            Column(children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 70, left: 5, right: 5),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text(
-                          "Favorite Contact",
-                          style: TextStyle(color: Colors.white, fontSize: 16),
-                        ),
-                        IconButton(
-                            onPressed: () {},
-                            icon: const Icon(
-                              Icons.more_horiz,
-                              color: Colors.white,
-                            ))
-                      ],
-                    ),
-                    SizedBox(
-                        height: 90,
-                        child: ListView(
-                            scrollDirection: Axis.horizontal,
-                            children: [
-                              buildContactAvatar("RO", "Ronaldo"),
-                              buildContactAvatar("EM", "Emerson"),
-                              buildContactAvatar("MA", "Marcelo"),
-                              buildContactAvatar("TH", "Thiago"),
-                              buildContactAvatar("SE", "Sergio"),
-                              buildContactAvatar("OU", "Outro"),
-                            ]))
+                    IconButton(
+                        onPressed: () {
+                          _globalKey.currentState!.openDrawer();
+                        },
+                        icon: const Icon(
+                          Icons.menu,
+                          color: Colors.white,
+                        )),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.search,
+                          color: Colors.white,
+                        ))
                   ],
                 ),
-              ))
-        ],
-      ),
+              ),
+              SizedBox(
+                height: 50,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  padding: const EdgeInsets.only(left: 10),
+                  children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text("Messages",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                    const SizedBox(width: 35),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text("Online",
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                    const SizedBox(width: 35),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text("Group",
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                    const SizedBox(width: 35),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text("More",
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold)),
+                    ),
+                    const SizedBox(width: 35),
+                  ],
+                ),
+              ),
+            ]),
+            Positioned(
+                top: 190,
+                left: 0,
+                right: 0,
+                child: Container(
+                  height: 220,
+                  padding: const EdgeInsets.only(top: 15, left: 25, right: 25),
+                  decoration: const BoxDecoration(
+                      color: Color(0xFF27c1a9),
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40),
+                          topRight: Radius.circular(40))),
+                  child: Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text(
+                            "Favorite Contact",
+                            style: TextStyle(color: Colors.white, fontSize: 16),
+                          ),
+                          IconButton(
+                              onPressed: () {},
+                              icon: const Icon(
+                                Icons.more_horiz,
+                                color: Colors.white,
+                              ))
+                        ],
+                      ),
+                      SizedBox(
+                          height: 90,
+                          child: ListView(
+                              scrollDirection: Axis.horizontal,
+                              children: [
+                                buildContactAvatar("RO", "Ronaldo"),
+                                buildContactAvatar("EM", "Emerson"),
+                                buildContactAvatar("MA", "Marcelo"),
+                                buildContactAvatar("TH", "Thiago"),
+                                buildContactAvatar("SE", "Sergio"),
+                                buildContactAvatar("OU", "Outro"),
+                              ]))
+                    ],
+                  ),
+                )),
+            Positioned(
+                top: 365,
+                left: 0,
+                right: 0,
+                bottom: 0,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(vertical: 15),
+                  decoration: const BoxDecoration(
+                      color: Color(0xFFEFFFFC),
+                      borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(40),
+                          topRight: Radius.circular(40))),
+                  child: ListView(
+                    padding: const EdgeInsets.only(left: 25),
+                    children: [
+                      buildConversationRow("TE", "Thiago", "Hello", 2),
+                      buildConversationRow("PE", "Pedro", "How are you", 1),
+                      buildConversationRow("RP", "Ronaldo", "Hi", 0),
+                    ],
+                  ),
+                ))
+          ],
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,
+        floatingActionButton: SizedBox(
+          height: 65,
+          width: 65,
+          child: FloatingActionButton(
+            backgroundColor: const Color(0xFF27c1a9),
+            onPressed: () {},
+            child: const Icon(
+              Icons.edit_outlined,
+              size: 30,
+            ),
+          ),
+        ),
+        // bottomNavigationBar: const CustomNavigationApp(),
+        drawer: const MenuDrawer());
+  }
 
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ),
-      // bottomNavigationBar: const CustomNavigationApp(),
-      // drawer: const MenuDrawer()
+  Column buildConversationRow(
+      String name, String fullName, String message, int msgCount) {
+    return Column(
+      children: [
+        const SizedBox(
+          height: 5,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                UserAvatar(name: name),
+                const SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      fullName,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      message,
+                      style: const TextStyle(color: Colors.black, fontSize: 16),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 25, top: 0),
+              child: Column(
+                children: [
+                  const Text(
+                    "11:12",
+                    style: TextStyle(fontSize: 12),
+                  ),
+                  const SizedBox(height: 10),
+                  if (msgCount > 0)
+                    CircleAvatar(
+                      radius: 10,
+                      backgroundColor: const Color(0xFF27c1a9),
+                      child: Text(
+                        msgCount.toString(),
+                        style:
+                            const TextStyle(color: Colors.white, fontSize: 12),
+                      ),
+                    )
+                ],
+              ),
+            )
+          ],
+        ),
+        const Divider(
+          indent: 70,
+        )
+      ],
     );
   }
 
   Padding buildContactAvatar(String name, String fullName) {
     return Padding(
-      padding: EdgeInsets.only(right: 15),
+      padding: const EdgeInsets.only(right: 15),
       child: Column(
         children: [
           UserAvatar(
--- lib/menuDrawer/menuDrawer.dart
+++ lib/menuDrawer/menuDrawer.dart
@@ -1,37 +1,101 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_jobtopgun/src/settings.dart';
 
+import '../userAvatar.dart';
+
 class MenuDrawer extends StatelessWidget {
   const MenuDrawer({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-        child: ListView(
-      padding: EdgeInsets.zero,
-      children: [
-        const DrawerHeader(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.zero),
-            color: Color.fromARGB(210, 133, 249, 212),
+      width: 285,
+      backgroundColor: Colors.black38,
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.horizontal(right: Radius.circular(40))),
+      child: Container(
+        decoration: const BoxDecoration(
+            color: Color(0xF71F1E1E),
+            borderRadius: BorderRadius.horizontal(right: Radius.circular(40)),
+            boxShadow: [
+              BoxShadow(
+                  color: Color(0x3D000000), blurRadius: 20, spreadRadius: 30)
+            ]),
+        child: const Padding(
+          padding: EdgeInsets.fromLTRB(20, 50, 20, 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.arrow_back_ios, color: Colors.white, size: 24),
+                      SizedBox(
+                        width: 56,
+                      ),
+                      Text("Settings",
+                          style: TextStyle(color: Colors.white, fontSize: 16)),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Row(
+                    children: [
+                      UserAvatar(name: "RP"),
+                      SizedBox(width: 20),
+                      Text(
+                        "Ratchanon P",
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 35,
+                  ),
+                  DrawerItem(title: "Account", icon: Icons.key),
+                  DrawerItem(title: "Chats", icon: Icons.chat_bubble),
+                  DrawerItem(title: "Notifications", icon: Icons.notifications),
+                  Divider(
+                    height: 35,
+                    color: Colors.green,
+                  ),
+                  DrawerItem(title: "Invite friends", icon: Icons.people),
+                ],
+              ),
+              DrawerItem(title: "Log out", icon: Icons.logout),
+            ],
           ),
-          child: Text('Drawer Header Title'),
         ),
-        ListTile(
-          title: const Text('Prodile'),
-          onTap: () {},
-        ),
-        ListTile(
-          title: const Text('About'),
-          onTap: () {},
-        ),
-        ListTile(
-          title: const Text('Setting'),
-          onTap: () {
-            Navigator.pushNamed(context, Settings.routeName);
-          },
-        ),
-      ],
-    ));
+      ),
+    );
+  }
+}
+
+class DrawerItem extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  const DrawerItem({
+    super.key,
+    required this.title,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 15, bottom: 15),
+      child: Row(
+        children: [
+          Icon(icon, color: Colors.white, size: 24),
+          const SizedBox(
+            width: 40,
+          ),
+          Text(title,
+              style: const TextStyle(color: Colors.white, fontSize: 16)),
+        ],
+      ),
+    );
   }
 }
